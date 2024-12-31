c47983f940d5cd231c85b181401330104c7a9753
Mark Traquair <marktraquair@gmail.com>
2024-12-31 16:10:38 -0500

    KeyEvents + plugin integration

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/aaa/KeyEvents.java
@@ -0,0 +1,29 @@
+package net.runelite.client.plugins.aaa;
+
+import net.runelite.client.RuneLite;
+import net.runelite.api.Client;
+import java.awt.event.KeyEvent;
+
+
+public class KeyEvents {
+    private static final Client client = RuneLite.getInjector().getInstance(Client.class);
+
+    public static void pressKey(int keyCode) {
+        if (keyCode == -1) {
+            keyCode = KeyEvent.VK_ENTER;
+        }
+        KeyEvent keyPress = new KeyEvent(client.getCanvas(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, keyCode, KeyEvent.CHAR_UNDEFINED);
+        KeyEvent keyRelease = new KeyEvent(client.getCanvas(), KeyEvent.KEY_RELEASED, System.currentTimeMillis(), 0, keyCode, KeyEvent.CHAR_UNDEFINED);
+        KeyEvent keyTyped = new KeyEvent(client.getCanvas(), KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char) keyCode);
+        try {
+            // Add random delay between 20-100ms for key press to release
+            // This simulates typical human key press duration
+            Thread.sleep((long)(20 + Math.random() * 80));
+        } catch (InterruptedException e) {
+            // Ignore interruption
+        }
+        client.getCanvas().dispatchEvent(keyPress);
+        client.getCanvas().dispatchEvent(keyRelease);
+        client.getCanvas().dispatchEvent(keyTyped);
+    }
+} 
--- runelite-client/src/main/java/net/runelite/client/plugins/aaa/NotAgility.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/aaa/NotAgility.java
@@ -24,71 +24,30 @@
  */
 package net.runelite.client.plugins.aaa;
 
-import com.google.common.collect.ImmutableSet;
-import com.google.inject.Provides;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.RescaleOp;
 import java.awt.Graphics2D;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.function.Consumer;
 import net.runelite.client.util.ImageUtil;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import javax.inject.Inject;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
+// import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
-import net.runelite.api.ItemID;
-import net.runelite.api.MenuEntry;
-import net.runelite.api.NPC;
 import net.runelite.api.Renderable;
-import net.runelite.api.Player;
-import net.runelite.api.Tile;
-import net.runelite.api.TileItem;
-import net.runelite.api.TileObject;
-import net.runelite.api.coords.WorldPoint;
-import net.runelite.api.events.DecorativeObjectDespawned;
-import net.runelite.api.events.DecorativeObjectSpawned;
-import net.runelite.api.events.GameObjectDespawned;
-import net.runelite.api.events.GameObjectSpawned;
-import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
-import net.runelite.api.events.GroundObjectDespawned;
-import net.runelite.api.events.GroundObjectSpawned;
-import net.runelite.api.events.ItemDespawned;
-import net.runelite.api.events.ItemSpawned;
-import net.runelite.api.events.NpcDespawned;
-import net.runelite.api.events.NpcSpawned;
-import net.runelite.api.events.StatChanged;
-import net.runelite.api.events.WallObjectDespawned;
-import net.runelite.api.events.WallObjectSpawned;
-import net.runelite.client.Notifier;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.events.ConfigChanged;
-import net.runelite.client.game.ItemManager;
 import net.runelite.client.input.MouseManager;
 import net.runelite.client.plugins.Plugin;
-import net.runelite.client.plugins.PluginDependency;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.client.plugins.aaa.aaaInputListener;
-import net.runelite.client.plugins.xptracker.XpTrackerPlugin;
-import net.runelite.client.plugins.xptracker.XpTrackerService;
 import net.runelite.client.ui.ClientUI;
-import net.runelite.client.ui.overlay.OverlayManager;
-import net.runelite.client.ui.overlay.infobox.InfoBoxManager;
-import net.runelite.client.util.Text;
 import py4j.GatewayServer;
-import java.awt.event.MouseEvent;
 import java.util.concurrent.ConcurrentLinkedQueue;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.chat.ChatCommandManager;
+import net.runelite.client.plugins.aaa.KeyEvents;
+
 
 @PluginDescriptor(
 	name = "AAAAAAAA",
@@ -100,16 +59,10 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class NotAgility extends Plugin
 {
 	@Inject
-	private OverlayManager overlayManager;
-
-	@Inject
 	private aaaInputListener inputListener;
 
 	@Inject
 	private MouseManager mouseManager;
-
-	@Inject
-	private Notifier notifier;
 
 	@Inject
 	@Getter
@@ -117,22 +70,26 @@ public class NotAgility extends Plugin
 
 	@Inject
 	@Getter
+	private ChatCommandManager chatCommandManager;
+
+	@Inject
+	@Getter
 	private ClientUI clientUI;
+
+	@Getter
+	private KeyEvents keyEvents = new KeyEvents();
 
 	@Getter
 	@Inject
 	private ConcurrentLinkedQueue<Renderable> clickQueue = new ConcurrentLinkedQueue<>();
-
-	@Inject
-	private ScheduledExecutorService executor;
 	
 	private GatewayServer server;
 
 	public int tickCount = 0;
 
 	public Image lastImage;
-	// @Inject
-	// private InfoBoxManager infoBoxManager;
+
+	static Client api_client = RuneLite.getInjector().getInstance(Client.class);
 
 	@Override
 	public void startUp()
@@ -154,12 +111,6 @@ public class NotAgility extends Plugin
 	{
 		tickCount++;
 		tickCount %= 1000;
-		Consumer<Image> imageCallback = (img) ->
-		{
-			// This callback is on the game thread, move to executor thread
-			executor.submit(() -> lastImage = img);
-		};
-		// TODO: Use this as the trigger for data tracking, either every tick or every other
 	}
 
 	/**
