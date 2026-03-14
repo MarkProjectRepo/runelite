98817d3f33e9640c799b27d1b4177f862f63e9d5
MarkProjectRepo <7595234+MarkProjectRepo@users.noreply.github.com>
2026-03-14 17:08:22 +0000

    feat: Add dynamic bindings for Py4J using __getattr__ and generate stubs

--- runelite-client/src/main/java/net/runelite/client/plugins/py4j/Py4jConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/py4j/Py4jConfig.java
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.client.plugins.Py4j;
+package net.runelite.client.plugins.py4j;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
